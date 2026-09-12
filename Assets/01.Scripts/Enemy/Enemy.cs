181d99294a2ec00acb270c2fae7d43bf30c53cbb
moha <djd468694@gmail.com>
2026-09-13 00:23:00 +0900

    enemy 틀

---
+++ Assets/01.Scripts/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+using Unity.Android.Gradle.Manifest;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public EnemyData _enemyData;
+    public int _hp = 0;
+    public int _getAttack
+    {
+        get
+        {
+            return Random.Range(_enemyData.MinAttack, _enemyData.MaxAttack+1);
+        }
+    }
+
+    public void Damage(int playerAtk)
+    {
+        int temp = 0;
+        temp = playerAtk - _enemyData.Defense;
+        if (temp > 0)
+            _hp -= temp;
+    }
+
+    private void Start()
+    {
+        _hp = _enemyData.MaxHP;
+    }
+
+}
