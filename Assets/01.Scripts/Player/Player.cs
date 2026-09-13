6571d23c280fc833eefe43e1e6f94bcd92859901
moha <djd468694@gmail.com>
2026-09-13 18:48:23 +0900

    방어 무기

--- Assets/01.Scripts/Player/Player.cs
+++ Assets/01.Scripts/Player/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     public int _hp;
     public SpriteRenderer _spriteRenderer;
     public int _defense;
+
+    public WeaponData _weapon;
+    public ShieldData _shield;
+
     private void Start()
     {
         _hp = _maxHp;
@@ -15,13 +19,29 @@ public class Player : MonoBehaviour
 
     public void Attack(Enemy target, int pinAtk)
     {
+        if (_weapon != null)
+        {
+            pinAtk = (int)(pinAtk * _weapon.DamageMultiplier);
+            pinAtk += (int)_weapon.BonusDamage;
+        }
+        //공격무기 효과 추가 및 방어력 무시 추가해야함
+
         _animator.SetTrigger("onAttack");
         target.Damage(pinAtk);
     }
     public void Damage(int enemyAtk)
     {
+        int defense = _defense;
+        if (_shield != null)
+            defense += (int)_shield.ShieldAmount;
+
         int temp;
-        temp = enemyAtk - _defense;
+        temp = enemyAtk - defense;
+        if(_shield != null)
+        {
+            temp = (int)(temp * (100 - _shield.DamageReduction) / 100);
+        }
+        // 실드 효과 추가해야함
         if (temp > 0)
         {
             _hp -= temp;
